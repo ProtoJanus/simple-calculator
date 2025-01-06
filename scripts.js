a9226e4e021178cb248b963598be0693701fdcdd
ProtoJanus <165827599+ProtoJanus@users.noreply.github.com>
2025-01-05 22:55:06 -0600

    Add clear button, and spacing between numbers and operands

--- scripts.js
+++ scripts.js
@@ -1,8 +1,18 @@
 const displayButtons = document.querySelectorAll(".display-button");
 const display = document.querySelector("#display");
+const clearButton = document.querySelector("#clear-button");
 
 displayButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
-        display.textContent = display.textContent + " " + button.textContent;
+        if(button.textContent === "+" || button.textContent === "-" || button.textContent === "/" || button.textContent === "*") {
+            display.textContent = display.textContent + " " + button.textContent + " ";
+            console.log("not a number");
+        }else{
+            display.textContent = display.textContent + button.textContent;
+        }
     })
+})
+
+clearButton.addEventListener("click", (e) => {
+    display.textContent = "";
 })
