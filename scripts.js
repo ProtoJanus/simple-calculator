b8c2c558d71249b4d02ec6c65ef6b533afe003eb
ProtoJanus <165827599+ProtoJanus@users.noreply.github.com>
2025-01-06 23:04:33 -0600

    Clean the event listener for the buttons, simplifying the if statements

--- scripts.js
+++ scripts.js
@@ -1,18 +1,37 @@
 const displayButtons = document.querySelectorAll(".display-button");
 const display = document.querySelector("#display");
 const clearButton = document.querySelector("#clear-button");
+const enterButton = document.querySelector("#enter-button");
 
+let isResult = false;
+
+// a display button is a button that will change the display in some way that isn't clearing or showing a result
+// in this case all operands and operators
 displayButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
-        if(button.textContent === "+" || button.textContent === "-" || button.textContent === "/" || button.textContent === "*") {
-            display.textContent = display.textContent + " " + button.textContent + " ";
-            console.log("not a number");
-        }else{
+        if (isResult) {
+            display.textContent = "";
+            isResult = false;
+        }
+
+        if (button.textContent === "+" || button.textContent === "-" || button.textContent === "/" || button.textContent === "*") {
+            if (display.textContent !== "") {
+                if (display.textContent.trim().slice(-1) !== "+" && display.textContent.trim().slice(-1) !== "-" && display.textContent.trim().slice(-1) !== "*" && display.textContent.trim().slice(-1) !== "/") {
+                    display.textContent = display.textContent + " " + button.textContent + " ";
+                }
+            }
+        } else {
             display.textContent = display.textContent + button.textContent;
         }
-    })
-})
+    });
+});
+
 
 clearButton.addEventListener("click", (e) => {
     display.textContent = "";
+})
+
+enterButton.addEventListener("click", (e) => {
+    display.textContent = eval(display.textContent);
+    isResult = true;
 })
